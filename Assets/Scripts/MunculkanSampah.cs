3ce9803d4e4f5b3b9b0d38183f4252ce393ab258
makarimachmad <makarimachmad@gmail.com>
2018-10-23 04:43:57 +0700

    upload game pilah sampah, ada kekurangan Dicoding Awal/suo, kalo ada eror tolong dicomment

---
+++ Assets/Scripts/MunculkanSampah.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class MunculkanSampah : MonoBehaviour
+{
+    public float jeda = 0.8f;
+    float timer;
+    public GameObject[] obyekSampah;
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > jeda)
+        {
+            int random = Random.Range(0, obyekSampah.Length);
+            Instantiate(obyekSampah[random], transform.position, transform.rotation);
+            timer = 0;
+        }
+    }
+}
+
